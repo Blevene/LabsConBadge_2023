34ada1e8df9cf9d3273ccf74b651bbb0896a03a0
Blevene <Blevene@users.noreply.github.com>
2023-08-03 10:17:32 -0400

    Menu Prototype
    Display a Text Based menu that can be navigated with a 5 way switch.

---
+++ prototype/logic/menu_proto.py
@@ -0,0 +1,66 @@
+import board
+import digitalio
+import busio
+import adafruit_ssd1306
+
+# iInitialize the display
+i2c = busio.I2C(board.SCL, board.SDA)
+display = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c)
+
+
+# Map the 5 way switch to pins
+up_button = digitalio.DigitalInOut(board.D0)
+up_button.direction = digitalio.Direction.INPUT
+up_button.pull = digitalio.Pull.UP
+
+down_button = digitalio.DigitalInOut(board.D2)
+down_button.direction = digitalio.Direction.INPUT
+down_button.pull = digitalio.Pull.UP
+
+left_button = digitalio.DigitalInOut(board.D1)
+left_button.direction = digitalio.Direction.INPUT
+left_button.pull = digitalio.Pull.UP
+
+right_button = digitalio.DigitalInOut(board.D9)
+right_button.direction = digitalio.Direction.INPUT
+right_button.pull = digitalio.Pull.UP
+
+select_button = digitalio.DigitalInOut(board.D7)
+select_button.direction = digitalio.Direction.INPUT
+select_button.pull = digitalio.Pull.UP
+
+
+# Define our Menu
+menu = ["Send", "Receive", "Show All Clues", "Send Answer"]
+current_position = 0
+
+# Build and Display our Menu on the OLED
+def draw():
+    display.fill(0)
+    display.text('Menu:', 0, 0, 1)
+    for i, option in enumerate(menu):
+        y = 10 + i*10
+        if i == current_position:
+            display.text('> ' + option, 0, y, 1)
+        else:
+            display.text('  ' + option, 0, y, 1)
+    display.show()
+
+# Main Loop
+# Note: ADD DEBOUNCE, its Polling the Button presses WAY too quickly
+while True:
+    if not up_button.value:  # move up
+        current_position = (current_position - 1) % len(menu)
+        draw()
+        
+
+    elif not down_button.value:  # move down
+        current_position = (current_position + 1) % len(menu)
+        draw()
+
+    elif not select_button.value:  # select
+        print("Selected: ", menu[current_position])
+        # Do The Needful Based on the value of menu[current_position]
+
+    elif not left_button.value or not right_button.value:  # do nothing, here for future use
+        pass
