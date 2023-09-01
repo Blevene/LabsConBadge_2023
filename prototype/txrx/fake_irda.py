63de453ce7741eb591c7f74a483ec640532ad881
@securelyfitz <joefitz@joefitz.net>
2023-09-01 01:35:31 -0700

    refactoring tx/rx into a module/class that wraps uart and does all the irda swizzle

---
+++ prototype/txrx/fake_irda.py
@@ -0,0 +1,35 @@
+import board
+import digitalio
+
+class FakeIRDA:
+    def __init__(self,uart=board.UART(),sd=board.D8):
+        self.uart=uart
+        self.uart.baudrate=400000
+        self.shutdown=digitalio.DigitalInOut(sd)
+        self.shutdown.switch_to_output()
+        self.shutdown.value=1
+
+    # reads two bytes from uart and repacks it into one byte of data and returns it
+    def readbyte(self):
+        self.enablePHY()
+        rxval=self.uart.read(2)
+        if rxval is not None:
+            return rxval[0] & (rxval[1]>>1|128)
+
+    # takes one byte, expands it to two bytes interleved with '1's and transmits both
+    def writebyte(self,txval):
+        self.enablePHY()
+        self.uart.write(bytes([txval|85,(((txval<<1)|85)%256)]))
+
+    # set shutdown pin to 0 to wake up phy 
+    def enablePHY(self):
+        self.shutdown.value=0
+
+    #set shutdown pint to 1 to shut down phy and save power
+    def disablePHY(self):
+        self.shutdown.value=1
+
+    #return true if uart has enough bytes waiting.
+    def ready(self):
+        return self.uart.in_waiting > 1
+
