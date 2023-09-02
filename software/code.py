8991f987d35770876e6cff967dd718fe93e3bcb0
@securelyfitz <joefitz@joefitz.net>
2023-09-01 22:38:55 -0700

    stub out trade logic, add read/write multiple bytes option

--- software/code.py
+++ software/code.py
@@ -5,6 +5,9 @@ import time
 import pulseio
 from five_way_pad import FiveWayPad
 from sh1106_ui import sh1106ui
+from adafruit_ticks import ticks_ms, ticks_add, ticks_less
+
+
 
 display=sh1106ui()
 dpad=FiveWayPad()
@@ -66,14 +69,56 @@ class settings:
         return "settings"
 
 class trade:
+    state="tx"
+    count=0
+    timeout=5
+    retries=3
+
     def __init__(self, display, dpad):
         self.dpad=dpad
         self.display=display
 
     def update(self):
-        self.display.header.text="trade"
-        if dpad.d.fell: return 0 # will return to last normal page
-        if dpad.x.fell: display.details.hidden=not display.details.hidden
+        #update display
+        display.header.text="trade"
+
+        # process keypresses
+        # if down is pressed, return to where we came from
+        if dpad.d.fell: return 0
+        # if x is pressed, restart the trade process
+        elif dpad.x.fell: 
+            state="tx"
+            count=0
+
+        # process tx/rx
+        # if state is tx, transmit and increment count
+        elif state is "tx":
+            print("transmitting", count)
+            #if afer transmitting a few times, prepare to recieve
+            if count++ > retries:
+                count=0
+                state="rx"
+                #uart.reset_input_buffer()
+                timeout=ticks_ms()+5000
+        # if state is rx, recieve until valid card recieved or timeout
+        elif state is "rx":
+            print "receiving"
+            # if uart ready
+                # read data
+                # parse data
+                # if data valid:
+                    # store data
+                    # state="respond"
+                # return "trade"
+            if ticks_ms()> timeout:
+                state="respond"
+        # else state is respond, tx 3 more times
+        else
+            print("transmitting", count)
+            if count++ > retries:
+                count=0
+                state="rx"
+                return 0
         return "trade"
 
 class sleep:
@@ -83,9 +128,12 @@ class sleep:
 
     def update(self):
         #self.display.header.text="sleep"
-        if dpad.u.fell:
+        if dpad.x.fell:
             display.splash.hidden=False
             return 0 # will return to last normal page
+        elif dpad.u.fell:
+            display.splash.hideden=False
+            return "trade"
         display.splash.hidden=True
         return "sleep"
 
@@ -102,7 +150,7 @@ while True:
     #scan inputs
     dpad.update()
     #go to sleep if timeout
-    if dpad.duration()>5 : page=sleeppage.update()
+    if dpad.duration()>5 and page is not "trade": page=sleeppage.update()
     #if a button is pressed, handle it
     elif not dpad.pressed():
         time.sleep(0.01)
--- software/lib/fake_irda.py
+++ software/lib/fake_irda.py
@@ -16,10 +16,23 @@ class FakeIRDA:
         if rxval is not None:
             return rxval[0] & (rxval[1]>>1|128)
 
+    # calls readbyte N times, reading 2*N bytes
+    def readbytes(self,count):
+        bytesread=[]
+        for i in range(count)
+            bytesread.append(readbyte())
+        return bytesread
+
     # takes one byte, expands it to two bytes interleved with '1's and transmits both
     def writebyte(self,txval):
         self.enablePHY()
         self.uart.write(bytes([txval|85,(((txval<<1)|85)%256)]))
+
+    # calls writebyte N times, writing 2N bytes total
+    def writebytes(self,byteswrite):
+        for i in byteswrite
+            writebyte(i)
+        return bytesread
 
     # set shutdown pin to 0 to wake up phy 
     def enablePHY(self):
