ba338aaa5992950fa0104f915c59d79e9ab0d921
@securelyfitz <joefitz@joefitz.net>
2023-09-19 19:19:53 -0700

    make user control phy enable/disable; keep buffer clear; cleanup print statements

--- software/lib/fake_irda.py
+++ software/lib/fake_irda.py
@@ -13,10 +13,10 @@ class FakeIRDA:
 
     # reads two bytes from uart and repacks it into one byte of data and returns it
     def readbyte(self):
-        self.enablePHY()
+        #self.enablePHY()
         rxval=self.uart.read(2)
         if (rxval is not None) and (len(rxval)==2):
-            print(rxval,rxval[0] & (rxval[1]>>1|128))
+            #print(rxval,rxval[0] & (rxval[1]>>1|128))
             return chr(rxval[0] & (rxval[1]>>1|128))
 
     # calls readbyte N times, reading 2*N bytes
@@ -26,7 +26,7 @@ class FakeIRDA:
             byte = self.readbyte()
             while byte is not None:
                 bytesread+=byte
-                print("read",byte,"one more byte?")
+                #print("read",byte,"one more byte?")
                 byte = self.readbyte()
                 pass
             return bytesread
@@ -36,9 +36,10 @@ class FakeIRDA:
 
     # takes one byte, expands it to two bytes interleved with '1's and transmits both
     def writebyte(self,txval):
-        self.enablePHY()
+        #self.enablePHY()
         self.uart.write(bytes([txval|85,(((txval<<1)|85)%256)]))
-        print(bytes([txval|85,(((txval<<1)|85)%256)]),txval)
+        #print(bytes([txval|85,(((txval<<1)|85)%256)]),txval)
+        self.uart.reset_input_buffer()
 
     # calls writebyte N times, writing 2N bytes total
     def writebytes(self,byteswrite):
