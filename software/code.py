29f404b2adb7aec262be75457aaa21b21d589878
@securelyfitz <joefitz@joefitz.net>
2023-09-21 16:08:21 -0700

    breaking out LEDcontrol class to separate file

--- software/code.py
+++ software/code.py
@@ -14,18 +14,7 @@ from five_way_pad import FiveWayPad
 from adafruit_display_text import label
 from adafruit_ticks import ticks_ms, ticks_add, ticks_less
 
-import neopixel
-from adafruit_led_animation.animation.blink import Blink
-from adafruit_led_animation.animation.solid import Solid
-from adafruit_led_animation.animation.colorcycle import ColorCycle
-from adafruit_led_animation.animation.chase import Chase
-from adafruit_led_animation.animation.comet import Comet
-from adafruit_led_animation.animation.pulse import Pulse
-from adafruit_led_animation.animation.rainbow import Rainbow
-from adafruit_led_animation.animation.rainbowchase import RainbowChase
-from adafruit_led_animation.animation.rainbowcomet import RainbowComet
-import adafruit_led_animation.color as color
-
+from leds import ledcontrol
 from home import home
 from clues import clues
 from alibis import alibis
@@ -39,49 +28,6 @@ WHITE=0xFFFFFF
 
 display=sh1106ui()
 dpad=FiveWayPad()
-
-class ledcontrol:
-    pixel_pin = board.NEOPIXEL
-    num_pixels = 10
-    colors = [color.RED,color.ORANGE,color.YELLOW,color.GREEN,color.BLUE,color.PURPLE,color.WHITE]
-    color_name = ["Red", "Orange", "Yellow", "Green", "Blue", "Purple", "White"]
-    color = colors[0]
-    brightnesslevels = [0.02, 0.15, 0.3, 0.5]
-    pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.3)
-    ledpatterns=[]
-    currentpattern=0
-
-    def __init__(self):
-        self.initcolors()
-
-    def initcolors(self):
-        self.ledpatterns = [
-            Solid(self.pixels,color.BLACK),
-            RainbowComet(self.pixels,0.1,ring=True),
-            Rainbow(self.pixels,0.1),
-            Chase(self.pixels,0.1,self.color),
-            Comet(self.pixels,0.1,self.color,tail_length=4,ring=True),
-            Solid(self.pixels,self.color),
-            Pulse(self.pixels,0.03,self.color),
-        ]
-
-    def nextbrightness(self):
-        self.pixels.brightness=self.brightnesslevels[(self.brightnesslevels.index(self.pixels.brightness) + 1) % len(self.brightnesslevels)]
-        bright = int(self.pixels.brightness*100)
-        return str(bright)+"%"
-
-    def nextpattern(self):
-        self.currentpattern=(self.currentpattern+1)%len(self.ledpatterns)
-        return self.ledpatterns[self.currentpattern].__qualname__[0:10]
-
-    def nextcolor(self):
-        self.color=self.colors[(self.colors.index(self.color) + 1) % len(self.colors)]
-        self.initcolors()
-        return self.color_name[self.colors.index(self.color)]
-
-    def animate(self):
-        self.ledpatterns[self.currentpattern].animate()
-
 
 leds=ledcontrol()
 
